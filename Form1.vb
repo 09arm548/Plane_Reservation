ab9640001695b60d793be461999ca18e7921deaf
09arm548 <arm20@sage.southhills.edu>
2010-10-03 06:05:52 -0400

    building GUI

---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub btnMap_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMap.Click
+        Dim strSeat(10, 4) As String
+
+    End Sub
+End Class
